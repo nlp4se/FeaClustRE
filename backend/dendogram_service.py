7276f80a00e6c7578496f14fa093a75ee2ebb4e0
mtiessler <max.tiessler@upc.edu>
2024-05-25 10:49:13 +0200

    added all strategies

--- backend/dendogram_service.py
+++ backend/dendogram_service.py
@@ -1,16 +1,29 @@
 import stanza
-from . import Context
+from .Context import Context
 from . import Affinity_strategy
+
 def generate_dendogram(preprocessing, embedding, features):
-    
     # TODO throw custom exception if embedding is none
     # TODO throw custom exception if preprocessing is none
     # TODO throw custom exception if features is none
     if preprocessing: 
         features = preprocess_features(features)
     context = None
+    
+    if embedding == 'levenshtein' or embedding == 'all':
+        context = Context(Affinity_strategy.TfIdfCosineAffinity())
+        context.use_affinity_algorithm(features)
+    
     if embedding == 'tf-idf-cosine' or embedding == 'all':
         context = Context(Affinity_strategy.TfIdfCosineAffinity())
+        context.use_affinity_algorithm(features)
+
+    if embedding == 'tf-idf-euclidean' or embedding == 'all':
+        context = Context(Affinity_strategy.TfIdfEuclideanAffinity())
+        context.use_affinity_algorithm(features)
+
+    if embedding == 'bert-embedding' or embedding == 'all':
+        context = Context(Affinity_strategy.BERTEmbeddingAffinity())
         context.use_affinity_algorithm(features)
     
 def preprocess_features(features):
@@ -22,6 +35,6 @@ def preprocess_features(features):
         for word in doc.sentences[0].words:
             lemmatized_feature = lemmatized_feature + word.lemma + " "
         preprocessed_features.append(lemmatized_feature[:len(lemmatized_feature) - 1])
-    return preprocess_features
+    return preprocessed_features
 
 
