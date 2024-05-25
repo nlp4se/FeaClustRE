33fd7aa2aa4582f3c5169969a81a020455f5807f
mtiessler <max.tiessler@upc.edu>
2024-05-25 10:36:22 +0200

    implemented lemmanization with Stanza

--- backend/dendogram_service.py
+++ backend/dendogram_service.py
@@ -9,13 +9,18 @@ def generate_dendogram(preprocessing, embedding, features):
     if preprocessing: 
         features = preprocess_features(features)
     context = None
-    if embedding == 'levenshtein':
-        context = Context(Affinity_strategy.LevenshteinAffinity())
+    if embedding == 'tf-idf-cosine' or embedding == 'all':
+        context = Context(Affinity_strategy.TfIdfCosineAffinity())
+        context.use_affinity_algorithm(features)
     
 def preprocess_features(features):
-    nlp_pipeline = stanza.Pipeline(lang='en', processors='lemma')
+    nlp_pipeline = stanza.Pipeline(lang='en', processors='tokenize,mwt,pos,lemma')
     preprocessed_features = []
     for feature in features:
-        preprocess_features.append(nlp_pipeline(feature))
+        doc = nlp_pipeline(feature)
+        lemmatized_feature = ""
+        for word in doc.sentences[0].words:
+            lemmatized_feature = lemmatized_feature + word.lemma + " "
+        preprocessed_features.append(lemmatized_feature[:len(lemmatized_feature) - 1])
 
 
