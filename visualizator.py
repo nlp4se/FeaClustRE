0fc6901c565600d907903b04713945cf792aec5a
mtiessler <max.tiessler@upc.edu>
2024-05-27 11:47:41 +0200

    fixes to visualizator and added metadata to file

--- visualizator.py
+++ visualizator.py
@@ -4,7 +4,11 @@ from scipy.cluster.hierarchy import dendrogram
 import joblib
 
 
-def plot_dendrogram(model, **kwargs):
+def add_line_breaks(labels):
+    return [label.replace(' ', '\n') for label in labels]
+
+
+def plot_dendrogram(model, labels, **kwargs):
     counts = np.zeros(model.children_.shape[0])
     n_samples = len(model.labels_)
     for i, merge in enumerate(model.children_):
@@ -19,18 +23,27 @@ def plot_dendrogram(model, **kwargs):
     linkage_matrix = np.column_stack(
         [model.children_, model.distances_, counts]
     ).astype(float)
-
-    dendrogram(linkage_matrix, **kwargs)
+    labels = add_line_breaks(labels=labels)
+    dendrogram(linkage_matrix,
+               labels=labels,
+               **kwargs,
+               show_leaf_counts=True,
+               orientation='bottom',
+               truncate_mode='level',
+               p=10)
+    plt.xticks(rotation=90, fontsize=10)
 
 
 def show_dendrogram(model_file):
-    model = joblib.load(model_file)
-
+    file = joblib.load(model_file)
+    model = file['model']
+    affinity = file['affinity']
+    labels = file['labels']
     if hasattr(model, 'children_'):
         plt.figure(figsize=(10, 6))
-        plot_dendrogram(model)
+        plot_dendrogram(model, labels=labels)
         # TODO dynamic name
-        plt.title('TF-IDF Cosine Distance Histogram')
+        plt.title(affinity)
         plt.xlabel('Features')
         plt.ylabel('Distance')
         plt.show()
