102a6509ec74a49c9bfdace813c2e0ae0b2fbfd1
mtiessler <maxtiessler4@gmail.com>
2025-07-07 08:57:46 +0200

    untracked changes

--- backend/tf_idf_utils.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-from sklearn.feature_extraction.text import TfidfVectorizer
-
-def get_dense_data_array(data: List):
-    tfidf_vectorizer = TfidfVectorizer()
-    tf_idf_data_vector = tfidf_vectorizer.fit_transform(data)
-    return tf_idf_data_vector.toarray()
--- data/model_embeddings/Readme.md
+++
@@ -1,2 +0,0 @@
-For computing cosine similarity between pair feature-feature1 and validate if they are synonym.
-
